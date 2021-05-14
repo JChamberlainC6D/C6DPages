c382577cf165674344157ea4520bdb8fa46a68f3
Jack <JChamberlain@carbonsix.digital>
2021-05-14 10:18:36 +0100

    Header Mobile optimisation

--- JS/js.js
+++ JS/js.js
@@ -5,3 +5,13 @@ $(function(){
 $(function(){
    $('#footer').load('footer.html');
  });
+
+
+ function showMenu() {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+  }
